20ec525c778c6f3a471720f56ce4b82ddf9dbf1f
lrpalmer27 <lrpalmer@mun.ca>
2023-12-13 09:48:34 -0330

    the 12.5m ice annotations didint save properly, potentially why it errroed out... uploading to graham and trying again

--- WorkingPOCs/TrainMRCNN_12p5Ice.py
+++ WorkingPOCs/TrainMRCNN_12p5Ice.py
@@ -39,13 +39,13 @@ class IceConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 4
+    IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 1 + 1 # Background + Ice + Ship
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 125 #100
+    STEPS_PER_EPOCH = 50 #100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
